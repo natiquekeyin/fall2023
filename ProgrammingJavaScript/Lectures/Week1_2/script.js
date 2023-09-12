7d9beaffead661483fd47561e3f1437914899f65
Noman Atique <noman.atique@keyin.com>
2023-09-12 12:42:47 -0400

    Sept 12

---
+++ ProgrammingJavaScript/Lectures/Week1_2/script.js
@@ -0,0 +1,30 @@
+// console.log("hello from script");
+// // alert("Hello I am annoying alert here");
+// var age = 20;
+// console.log("Welcome Alan you are ", age, "years old");
+
+// var customerName;
+// var CustomerName;
+
+// if (age > 20) {
+//   console.log("Wclcome");
+// } else {
+//   console.log("Underage");
+// }
+
+//block of statement }
+//this is comment
+/* this is
+
+multiline  commentss */
+
+// var x;
+// let y;
+
+var fname;
+fname = "Keyin";
+
+console.log(typeof fname);
+
+fname = 100;
+console.log(typeof fname);
