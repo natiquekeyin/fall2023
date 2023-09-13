b23e41698f144f8f5ae081231ed70fd3344dfb17
Noman Atique <noman.atique@keyin.com>
2023-09-13 12:22:32 -0400

    JavaScript Sept 13

--- ProgrammingJavaScript/Lectures/Week1_2/script.js
+++ ProgrammingJavaScript/Lectures/Week1_2/script.js
@@ -21,10 +21,87 @@ multiline  commentss */
 // var x;
 // let y;
 
-var fname;
-fname = "Keyin";
+// var fname;
+// fname = "Keyin";
 
-console.log(typeof fname);
-alert("hello");
-fname = 100;
-console.log(typeof fname);
+// console.log(typeof fname);
+// // alert("hello");
+// fname = 100;
+// console.log(typeof fname);
+
+// Operators: + - * / % (Binary )
+
+// Increment/Decrement ++, -- (unary) i++, ++i, --x, x--
+// Pre-increment, Post-increment
+
+// var x = 10;
+// console.log(x++);
+// console.log(++x);
+// console.log(--x);
+// console.log(x--);
+// console.log(x);
+
+// var x = 10;
+// x += 20;
+// console.log(x);
+
+// var number = 10;
+// // coercion is converting either side's type and comparing..
+// if (number === "10") {
+//   console.log("ten");
+// } else {
+//   console.log("Not ten");
+// }
+
+// Ternary Operator!
+
+// var age = 20;
+// if (age > 18) {
+//   console.log("Welcome");
+// } else {
+//   console.log("Underage");
+// }
+
+// conditon ? statement_if_true: statment_if_false;
+
+// var age = 2;
+// age > 18 ? console.log("Welcome") : console.log("Underage");
+
+// var age = 20;
+// var message;
+
+// message = age > 18 ? "Welcome" : "Underage";
+
+// console.log(message);
+
+// console.log("hello");
+// console.log("world");
+
+// var i;
+// for (i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// function keyin() {
+//   console.log("Keyin COllege");
+// }
+
+// keyin();
+
+// function juicer() {
+//   return juice;
+// }
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// console.log(add(5, 10));
+
+// function add(a, b) {
+//   var c = a + b;
+//   console.log(c);
+//   return c;
+// }
+
+// console.log(add(5, 10));
