216cefac5e2f2d61eb41ff17762596e213fadc25
Noman Atique <noman.atique@keyin.com>
2023-10-06 10:32:52 -0400

    fridayOct6

--- ProgrammingJavaScript/Lectures/Week5/script.js
+++ ProgrammingJavaScript/Lectures/Week5/script.js
@@ -171,86 +171,86 @@
 // alan.login();
 // console.log(alan);
 
-let users = [
-  {
-    firstName: "Ala",
-    lastName: "Smith",
-    email: "alan@gmail.com",
-    loggedIn: false,
-    level: 3,
-    login: function () {
-      this.loggedIn = true;
-    },
-    logout: function () {
-      this.loggedIn = false;
-    },
-  },
-  {
-    firstName: "Bob",
-    lastName: "Smith",
-    email: "bob@gmail.com",
-    loggedIn: true,
-    level: 1,
-    login: function () {
-      this.loggedIn = true;
-    },
-    logout: function () {
-      this.loggedIn = false;
-    },
-  },
-  {
-    firstName: "tina                                           ",
-    lastName: "Smith",
-    email: "christina@gmail.com",
-    loggedIn: false,
-    level: 4,
-    login: function () {
-      this.loggedIn = true;
-    },
-    logout: function () {
-      this.loggedIn = false;
-    },
-  },
-  {
-    firstName: "Christina",
-    lastName: "Smith",
-    email: "christina@gmail.com",
-    loggedIn: true,
-    level: 4,
-    login: function () {
-      this.loggedIn = true;
-    },
-    logout: function () {
-      this.loggedIn = false;
-    },
-  },
-  {
-    firstName: "Chr",
-    lastName: "Smith",
-    email: "christina@gmail.com",
-    loggedIn: false,
-    level: 4,
-    login: function () {
-      this.loggedIn = true;
-    },
-    logout: function () {
-      this.loggedIn = false;
-    },
-  },
-  {
-    firstName: "Christina",
-    lastName: "Smith",
-    email: "christina@gmail.com",
-    loggedIn: false,
-    level: 4,
-    login: function () {
-      this.loggedIn = true;
-    },
-    logout: function () {
-      this.loggedIn = false;
-    },
-  },
-];
+// let users = [
+//   {
+//     firstName: "Ala",
+//     lastName: "Smith",
+//     email: "alan@gmail.com",
+//     loggedIn: false,
+//     level: 3,
+//     login: function () {
+//       this.loggedIn = true;
+//     },
+//     logout: function () {
+//       this.loggedIn = false;
+//     },
+//   },
+//   {
+//     firstName: "Bob",
+//     lastName: "Smith",
+//     email: "bob@gmail.com",
+//     loggedIn: true,
+//     level: 1,
+//     login: function () {
+//       this.loggedIn = true;
+//     },
+//     logout: function () {
+//       this.loggedIn = false;
+//     },
+//   },
+//   {
+//     firstName: "tina                                           ",
+//     lastName: "Smith",
+//     email: "christina@gmail.com",
+//     loggedIn: false,
+//     level: 4,
+//     login: function () {
+//       this.loggedIn = true;
+//     },
+//     logout: function () {
+//       this.loggedIn = false;
+//     },
+//   },
+//   {
+//     firstName: "Christina",
+//     lastName: "Smith",
+//     email: "christina@gmail.com",
+//     loggedIn: true,
+//     level: 4,
+//     login: function () {
+//       this.loggedIn = true;
+//     },
+//     logout: function () {
+//       this.loggedIn = false;
+//     },
+//   },
+//   {
+//     firstName: "Chr",
+//     lastName: "Smith",
+//     email: "christina@gmail.com",
+//     loggedIn: false,
+//     level: 4,
+//     login: function () {
+//       this.loggedIn = true;
+//     },
+//     logout: function () {
+//       this.loggedIn = false;
+//     },
+//   },
+//   {
+//     firstName: "Christina",
+//     lastName: "Smith",
+//     email: "christina@gmail.com",
+//     loggedIn: false,
+//     level: 4,
+//     login: function () {
+//       this.loggedIn = true;
+//     },
+//     logout: function () {
+//       this.loggedIn = false;
+//     },
+//   },
+// ];
 
 // console.log(users[0].firstName);
 // console.log(users[1].firstName);
@@ -273,8 +273,172 @@ let users = [
 // let newUsers = users.map((user) => user.email.startsWith("c"));
 // console.log(newUsers);
 
-users.forEach((user) => {
-  if (user.firstName.length > 5) {
-    console.log(user);
-  }
-});
+// users.forEach((user) => {
+//   if (user.firstName.length > 5) {
+//     console.log(user);
+//   }
+// });
+
+// function User(f, l, e, v) {
+//   this.firstName = f;
+//   this.lastName = l;
+//   this.email = e;
+//   this.level = v;
+// }
+
+// User.prototype.department = "HR";
+// User.prototype.loggedIn = false;
+// User.prototype.login = function () {
+//   this.loggedIn = true;
+// };
+
+// User.prototype.logout = function () {
+//   this.loggedIn = false;
+// };
+// let obj1 = new User("Brad", "Green", "brad@gmail.com", 3);
+
+// // console.log(obj1.department);
+
+// obj1.department = "IT";
+// obj1.loggedIn = "true";
+// console.log(obj1);
+
+// function User(f, l, e, v) {
+//   this.firstName = f;
+//   this.lastName = l;
+//   this.email = e;
+//   this.level = v;
+//   this.hobbies = ["reading", "gym"];
+//   this.pref = {
+//     color: "red",
+//     model: "Audi",
+//   };
+// }
+
+// var obj1 = new User("Alan", "Smith", "alan@gmail.com", 3);
+// console.log(obj1.pref.model);
+
+// Example:
+
+// var people = [];
+// // constructor function Person..
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// function addPerson(name, age) {
+//   var p = new Person(name, age);
+//   people.push(p); //insert the newly created person in people array
+// }
+
+// addPerson("George", 32);
+// addPerson("Christina", 31);
+// addPerson("Doug", 25);
+
+// console.log(people);
+
+// Write a function called totalAge() that calculates the total age of all people in array...
+// [Vanessa]
+// function totalAge() {
+//   var total = 0;
+//   people.forEach(function (person) {
+//     if (person.age) {
+//       total += person.age;
+//     }
+//   });
+//   return total;
+// }
+// var total = totalAge();
+// console.log("Total Age:", total);
+
+// function totalAge() {
+//   var total = 0;
+//   for (var i = 0; i < people.length; i++) {
+//     total += people[i].age;
+//   }
+//   return total;
+// }
+// var total = totalAge();
+// console.log("Total Age:", total);
+
+// function totalAge() {
+//   var total = 0;
+//   people.forEach((person) => {
+//     total += person.age;
+//   });
+//   return total;
+// }
+// var total = totalAge();
+// console.log("Total Age:", total);
+
+// [Evan Harte]
+// function totalAge(array) {
+//   var total = 0;
+//   for (i = 0; i < array.length; i++) {
+//     total += array[i].age;
+//   }
+//   return total;
+// }
+
+// console.log(totalAge(people));
+
+var books = [
+  { title: "Good morning", author: "Stephen", isbn: 1234, readingStatus: true },
+  {
+    title: "Good Afternoon",
+    author: "Chris",
+    isbn: 3234,
+    readingStatus: false,
+  },
+  { title: "Good night", author: "Braden", isbn: 1134, readingStatus: true },
+];
+
+// Write a function that tells about the reading status of each book like following: "Already Read Good morning by Stephen", "You still need to read Good Afternoon by Chris"
+
+// [Scot's solution]
+// function readingStatus() {
+//   books.forEach((book) => {
+//     if (book.readingStatus == true) {
+//       console.log("Already read", book.title, " by", book.author);
+//     } else {
+//       console.log("You still need to read", book.title, " by", book.author);
+//     }
+//   });
+// }
+
+// function readingStatus() {
+//   books.forEach((book) => {
+//     book.readingStatus === true
+//       ? console.log(`Already read ${book.title} by ${book.author}`)
+//       : console.log(`You still need to read ${book.title} by ${book.author}`);
+//   });
+// }
+
+// readingStatus();
+
+// function Vehicle(make, model, year) {
+//   this.make = make;
+//   this.model = model;
+//   this.year = year;
+//   this.displayDetails = function () {
+//     console.log(`Make ${this.make}: Model ${this.model}: Year ${this.year}`);
+//   };
+// }
+
+// var elantra = new Vehicle("Hyundai", "Elantra", 2010);
+
+// elantra.displayDetails();
+
+var books = [
+  { title: "Good morning", author: "Stephen", isbn: 1234, readingStatus: true },
+  {
+    title: "Good Afternoon",
+    author: "Chris",
+    isbn: 3234,
+    readingStatus: false,
+  },
+  { title: "Good night", author: "Braden", isbn: 1134, readingStatus: true },
+];
+
+// Write a function that takes the book object as parameter and stores the new book in above books array..
