e5cb6eb1bb6c5b9979ec98ff2014bc85b1c33b7b
Noman Atique <noman.atique@keyin.com>
2024-02-15 11:53:20 -0500

    feb15

---
+++ WINTER2024/FrontendDevelopment/Week7/scripts/forms.js
@@ -0,0 +1,17 @@
+let form = document.querySelector("#registration");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  let name = document.querySelector("#name").value;
+  let email = document.querySelector("#email").value;
+
+  document.querySelector("#nm").innerText = parseInt(name) + parseInt(email);
+
+  //   document.querySelector("#nm").innerText = name.toUpperCase();
+  //   document.querySelector("#em").innerText = email;
+
+  e.preventDefault();
+}
+
+// the value alwyas comes out as string.. so you will always need to convert it if doing a mathematical calculation..
--- WINTER2024/FrontendDevelopment/Week7/scripts/tree.js
+++ WINTER2024/FrontendDevelopment/Week7/scripts/tree.js
@@ -1,4 +1,5 @@
 let tree = document.querySelector("#tree");
+let body = document.body;
 
 tree.addEventListener("click", function (e) {
   tree.src = loadTreeUrl();
@@ -15,6 +16,51 @@ function loadTreeUrl() {
   return links[currentUrl];
 }
 
-setInterval(() => {
-  tree.src = loadTreeUrl();
-}, 1000);
+body.addEventListener("click", function (e) {
+  document.querySelector("#posX").innerText = e.clientX;
+
+  document.querySelector("#posY").innerText = e.clientY;
+});
+
+let startButton = document.querySelector("#btn-start");
+let endButton = document.querySelector("#btn-end");
+let timerId;
+
+startButton.onclick = function (e) {
+  // if the user clics it more than once, ignore it once it's running
+  if (timerId) {
+    return;
+  }
+
+  let currentDate = document.querySelector("#p1");
+  currentDate.removeAttribute("hidden");
+
+  timerId = setInterval(function () {
+    let now = new Date();
+    currentDate.innerHTML = now.toLocaleString();
+  }, 1000);
+};
+
+endButton.onclick = function (e) {
+  if (!timerId) {
+    return;
+  }
+
+  // stop the timer..
+  clearInterval(timerId);
+  timerId = null;
+};
+
+// to get urls from links.js to the page..
+
+let picBtn = document.querySelector("#pics");
+
+picBtn.onclick = function (e) {
+  let ol = document.createElement("ol");
+  links.forEach((url) => {
+    let li = document.createElement("li");
+    li.innerHTML = `<a href="${url}"> <img src="${url}" width="200" alt="pic" class="thumb"/> ${url} </a>`;
+    ol.appendChild(li);
+  });
+  document.querySelector("#urls").appendChild(ol);
+};
