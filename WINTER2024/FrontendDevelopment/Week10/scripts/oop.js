b49ef9a53542fd55c1b40ddb467d62ed2df8a1bd
Noman Atique <noman.atique@keyin.com>
2024-03-06 08:38:38 -0500

    march6

---
+++ WINTER2024/FrontendDevelopment/Week10/scripts/oop.js
@@ -0,0 +1,9 @@
+class Book {
+  // a special method called constructor!
+
+  constructor() {
+    this.title = "Default";
+    this.author = "Default";
+    this.isbn = "Default";
+  }
+}
--- WINTER2024/FrontendDevelopment/Week10/scripts/script.js
+++ WINTER2024/FrontendDevelopment/Week10/scripts/script.js
@@ -16,14 +16,15 @@ window.addEventListener("DOMContentLoaded", function () {
   // addBookToList() will create a new row in the table on html page...
   function addBookToList(t, a, i) {
     if (t === "" || a === "" || i === "") {
-      alert("No field should be empty");
+      showAlert("No field should be empty", "error");
     } else {
       var row = document.createElement("tr");
-      row.innerHTML = `<td>${t}</td><td>${a}</td><td>${i}</td>`;
+      row.innerHTML = `<td>${t}</td><td>${a}</td><td>${i}</td><td><button class="delete">X</button></td>`;
 
       // console.log(row);
 
       document.querySelector("#book-list").appendChild(row);
+      showAlert("Book successfully added", "success");
     }
   }
 
@@ -32,4 +33,40 @@ window.addEventListener("DOMContentLoaded", function () {
     document.querySelector("#author").value = "";
     document.querySelector("#isbn").value = "";
   }
+
+  function showAlert(m, c) {
+    let div = document.createElement("div");
+    div.innerHTML = `<p>${m}</p>`;
+    // console.log(div);
+    div.className = c;
+    div.id = "box";
+    let container = document.querySelector(".container");
+    container.insertBefore(div, form);
+
+    setTimeout(function () {
+      document.querySelector("#box").remove();
+    }, 1000);
+  }
+
+  // to catch the delete button, associate a click event to the data area...
+
+  document.querySelector("#data").addEventListener("click", function (evt) {
+    // console.log(evt.target);
+    deleteBook(evt.target);
+  });
+
+  function deleteBook(elemToDelete) {
+    // console.log(elemToDelete);
+
+    if (elemToDelete.className === "delete") {
+      // console.log("Right area");
+
+      elemToDelete.parentElement.parentElement.remove();
+      showAlert("Book deleted successfully", "success");
+    } else {
+      showAlert("You clicked wrong area", "error");
+    }
+  }
 });
+
+// Convert above functional code to the Object oriented code! Means we want to introduce the concept of "CLASSES" and objects in JavaScript!... OOP...
