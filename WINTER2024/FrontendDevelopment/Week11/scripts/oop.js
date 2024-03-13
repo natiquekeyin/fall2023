dc484b1453bb0a58377eb3effc8b5f5027e89ede
Noman Atique <noman.atique@keyin.com>
2024-03-13 08:21:39 -0400

    march13

--- WINTER2024/FrontendDevelopment/Week11/scripts/oop.js
+++ WINTER2024/FrontendDevelopment/Week11/scripts/oop.js
@@ -1,3 +1,5 @@
+let form = document.querySelector("#book-form");
+
 class Book {
   constructor(t, a, i) {
     this.title = t;
@@ -18,12 +20,67 @@ class Book {
     document.querySelector("#isbn").value = "";
   }
 
-  showAlert() {}
+  showAlert(m, c) {
+    let div = document.createElement("div");
+    div.innerHTML = `<p>${m}</p>`;
+    // console.log(div);
+    div.className = c;
+    div.id = "box";
+    let container = document.querySelector(".container");
+    container.insertBefore(div, form); // insert div before form
+
+    setTimeout(function () {
+      document.querySelector("#box").remove();
+    }, 1000);
+  }
+
+  deleteBook(elemToDelete) {
+    if (elemToDelete.className === "delete") {
+      // console.log("Right area");
+
+      elemToDelete.parentElement.parentElement.remove();
+      this.showAlert("Book deleted successfully", "success");
+    } else {
+      this.showAlert("You clicked wrong area", "error");
+    }
+  }
 }
 
-window.addEventListener("DOMContentLoaded", function (e) {
-  let form = document.querySelector("#book-form");
+// Store class will be used to stroe and retrieve teh books we have added to the local storage...
 
+class Store {
+  static addBook(book) {
+    var books = Store.getBooks(); //read the memory if there are any books stored already
+    books.push(book); //push the "book" to the books array you got in last step
+
+    localStorage.setItem("books", JSON.stringify(books)); //we need to convert our array to json to store in memroy- stringify() converts a normal array to JSON array...{"name":"ALan"}
+  }
+  static getBooks() {
+    var books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books")); //parse() method converts teh json data into normal java script objects...
+    }
+    return books;
+  }
+  static displayBooks() {
+    var books = Store.getBooks();
+    books.forEach((book) => {
+      var objBook = new Book();
+      objBook.addBookToList(book);
+    });
+  }
+  static removeBooks(remBook) {
+    // read the books from memory..
+    // filter the books by leaving out the book passed as paramter "remBook"
+  }
+}
+
+// first I will need to create object of this calss..
+
+window.addEventListener("DOMContentLoaded", function (e) {
+  Store.displayBooks();
   form.addEventListener("submit", function (e) {
     var title = document.querySelector("#title").value;
     var author = document.querySelector("#author").value;
@@ -31,13 +88,22 @@ window.addEventListener("DOMContentLoaded", function (e) {
 
     var book = new Book(title, author, isbn);
     if (book.title === "" || book.author === "" || book.isbn === "") {
-      alert("no field shoud be empty");
+      book.showAlert("no field shoud be empty", "error");
     } else {
       book.addBookToList(book); //call the addBookToList() mehtod here...
       book.clearFields();
+      Store.addBook(book);
+      book.showAlert("Book successfully added", "success");
     }
 
     e.preventDefault();
+  });
+
+  document.querySelector("#data").addEventListener("click", function (evt) {
+    var book = new Book(); //create this object to get access to delteBook() method
+    book.deleteBook(evt.target);
+    Store.removeBooks(evt.target); //this call will remove the book from the local storage by calling remvoeBook() method from Store class...
+    evt.preventDefault();
   });
 });
 
