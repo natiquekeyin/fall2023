f32ad8a9d41da887ebb4852e651b28240c38f1f1
Noman Atique <noman.atique@keyin.com>
2024-03-28 11:33:34 -0400

    march28

---
+++ WINTER2024/ProgramminginJavaScript/Week14/testing/src/components/temp.js
@@ -0,0 +1,14 @@
+const addPeriod = (s) => {
+  return s + "!";
+};
+
+const actual = addPeriod("Keyin"); //return Keyin.
+const expected = "Keyin!";
+
+try {
+  if (actual !== expected) {
+    throw new Error("Test failed");
+  }
+} catch (s) {
+  console.log(s);
+}
